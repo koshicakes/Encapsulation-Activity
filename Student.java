ba6fc10c48caf221e86e2bc1816f2deebbb6a972
jorossmorales <jorossmorales042@gmail.com>
2026-02-22 22:51:54 +0800

    Student Code, Builder

--- Student.java
+++ Student.java
@@ -54,6 +54,7 @@ public final class Student {
                 " | Age: " + age +
                 " | " + course +
                 " | Year " + yearLevel +
+                 " | Address: " + address +
                 " | " + contactNumber;
     }
 
